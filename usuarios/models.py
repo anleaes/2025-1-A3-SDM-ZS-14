ae53114f9e987039be39c8fc42e4ffbc6be6b0aa
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:41:54 -0300

    usuario to string

--- usuarios/models.py
+++ usuarios/models.py
@@ -9,3 +9,7 @@ class Usuario(models.Model):
     sexo = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Feminino')])
     email = models.EmailField(null = False)
     telefone = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return f'{self.nome} - {self.cpf}'
+    
