aec93172a083493d168b91ef0c683b6b19cd8939
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:40:53 -0300

    imports + campanha vacinal

--- campanha_vacinal_vacina/models.py
+++ campanha_vacinal_vacina/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from campanhas_vacinais.models import CampanhaVacinal
+
 # Create your models here.
+
+class CampanhaVacinalVacina(models.Model):
+    campanha = models.ForeignKey(CampanhaVacinal, on_delete=models.CASCADE)
+    vacina = models.ForeignKey('vacinas.Vacina', on_delete=models.CASCADE)
