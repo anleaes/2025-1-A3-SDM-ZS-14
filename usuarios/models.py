ab422d54b07ac36e52a7841b9e1466cf05aaec4a
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:41:40 -0300

    usuarios models

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length= 100, null= False)
+    cpf = models.CharField(max_length= 20, null = False, unique= True)
+    data_nascimento = models.DateField(null = False)
+    sexo = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Feminino')])
+    email = models.EmailField(null = False)
+    telefone = models.CharField(max_length= 20)
