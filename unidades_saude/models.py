184a0ef79b83d595b670934ecf35a88ec5fa2024
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:04:33 -0300

    to string

--- unidades_saude/models.py
+++ unidades_saude/models.py
@@ -6,3 +6,7 @@ class UnidadeSaude(models.Model):
     nome = models.CharField(max_length= 100)
     endereco = models.CharField(max_length= 100)
     telefone = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return f'{self.nome} - {self.endereco}'
+    
