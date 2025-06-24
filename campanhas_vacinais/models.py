6fcedff67ace87f3a6c446dfaca9dcc3336b2caa
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:33:55 -0300

    import + campanha vacinal

--- campanhas_vacinais/models.py
+++ campanhas_vacinais/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class CampanhaVacinal(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=200)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
