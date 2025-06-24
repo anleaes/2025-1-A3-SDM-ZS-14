29d74e41325140e462491bd7682b7095876c9f00
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:28:34 -0300

    Vacina class

--- vacinas/models.py
+++ vacinas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Vacina(models.Model):
+    nome = models.CharField(max_length= 100)
+    descricao = models.CharField(max_length= 200)
+    fabricante = models.CharField(max_length= 100)
+    doses_recomendadas = models.IntegerField()
