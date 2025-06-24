f0bedd5903b7c0808f9f7d8295ef9e4056158917
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:49:33 -0300

    medicos

--- medicos/models.py
+++ medicos/models.py
@@ -3,3 +3,7 @@ from django.db import models
 from usuarios.models import Usuario
 
 # Create your models here.
+
+class Medico(Usuario):
+    registro_profissional = models.CharField(max_length=50, unique=True)
+    especialidade = models.CharField(max_length=100)
