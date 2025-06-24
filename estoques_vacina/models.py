8ee10e5ff687b510791592687222466e068e6ef4
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:12:57 -0300

    models

--- estoques_vacina/models.py
+++ estoques_vacina/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from unidades_saude.models import UnidadeSaude
+from vacinas.models import Vacina
+
 # Create your models here.
+
+class EstoqueVacina(models.Model):
+    quantidade_disponsivel = models.DecimalField(max_digits=10, decimal_places=2)
+    vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    unidade = models.ForeignKey(UnidadeSaude, on_delete=models.CASCADE)
