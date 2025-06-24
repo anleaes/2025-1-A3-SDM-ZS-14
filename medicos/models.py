aba39b93f06888007bb767947079065bb20022d7
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:49:46 -0300

    to string

--- medicos/models.py
+++ medicos/models.py
@@ -7,3 +7,6 @@ from usuarios.models import Usuario
 class Medico(Usuario):
     registro_profissional = models.CharField(max_length=50, unique=True)
     especialidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} - {self.especialidade}'
