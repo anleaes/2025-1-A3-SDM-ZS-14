c474a3eab19017d492b85158b3f13e3d34d2967e
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:43:42 -0300

    imports

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from usuarios.models import Usuario
+from usuarios.serializers import UsuarioSerializer
