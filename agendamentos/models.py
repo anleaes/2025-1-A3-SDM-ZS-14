47b67bbb80589c67dd28e6451cc8e767e1040d8c
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:47:46 -0300

    initial

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+    STATUS_CHOICES = [
+        ('pendente', 'Pendente'),
+        ('confirmado', 'Confirmado'),
+        ('cancelado', 'Cancelado'),
+        ('realizado', 'Realizado'),
+    ]
+
+    data_hora = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendente')
+
+    usuario = models.ForeignKey(
+        'usuarios.Usuario',
+        on_delete=models.CASCADE,
+    )
+    
+    vacina = models.ForeignKey(
+        'vacinas.Vacina',
+        on_delete=models.CASCADE,
+    )
