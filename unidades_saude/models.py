32090424ee2ed4e7690a7f35b657584aede6971e
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:04:14 -0300

    models

--- unidades_saude/models.py
+++ unidades_saude/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UnidadeSaude(models.Model):
+    nome = models.CharField(max_length= 100)
+    endereco = models.CharField(max_length= 100)
+    telefone = models.CharField(max_length= 100)
