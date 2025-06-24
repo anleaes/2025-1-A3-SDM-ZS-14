9d40b08c418b808000332d597e9411da3cb84402
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:27:40 -0300

    viewset

--- aplicacoes_vacinais/views.py
+++ aplicacoes_vacinais/views.py
@@ -1,3 +1,7 @@
 from rest_framework import viewsets
 from .models import AplicacaoVacina
 from .serializers import AplicacaoVacinaSerializer
+
+class AplicacaoVacinaViewSet(viewsets.ModelViewSet):
+    queryset = AplicacaoVacina.objects.all()
+    serializer_class = AplicacaoVacinaSerializer
