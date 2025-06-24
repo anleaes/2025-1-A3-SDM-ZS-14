f88e743409d8c861e9a492cbd55c18d3516dd8b8
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:50:14 -0300

    viewset

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -2,3 +2,8 @@ from rest_framework import viewsets
 
 from agendamentos.models import Agendamento
 from agendamentos.serializers import AgendamentoSerializer
+
+
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
