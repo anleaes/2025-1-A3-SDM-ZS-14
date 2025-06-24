2b838aec737e144b85caec02ef42bad85580d6f0
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:32:32 -0300

    vacinaviewset

--- vacinas/views.py
+++ vacinas/views.py
@@ -3,3 +3,8 @@ from rest_framework import viewsets
 
 from vacinas.models import Vacina
 from vacinas.serializers import VacinaSerializer
+
+
+class VacinaViewSet(viewsets.ModelViewSet):
+    queryset = Vacina.objects.all()
+    serializer_class = VacinaSerializer
