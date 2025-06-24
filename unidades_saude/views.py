4ba1ef93f68afef917daa70145bbf5211830016e
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:08:05 -0300

    viewset

--- unidades_saude/views.py
+++ unidades_saude/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 
 from unidades_saude.models import UnidadeSaude
 from unidades_saude.serializers import UnidadeSaudeSerializer
+
+class UnidadeSaudeViewSet(viewsets.ModelViewSet):
+    queryset = UnidadeSaude.objects.all()
+    serializer_class = UnidadeSaudeSerializer
