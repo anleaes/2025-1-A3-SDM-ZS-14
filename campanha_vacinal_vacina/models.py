f04de6d5bdc6bccd56893ff2c1c675f078a457ea
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:41:09 -0300

    meta + to string

--- campanha_vacinal_vacina/models.py
+++ campanha_vacinal_vacina/models.py
@@ -7,3 +7,9 @@ from campanhas_vacinais.models import CampanhaVacinal
 class CampanhaVacinalVacina(models.Model):
     campanha = models.ForeignKey(CampanhaVacinal, on_delete=models.CASCADE)
     vacina = models.ForeignKey('vacinas.Vacina', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('campanha', 'vacina')
+
+    def __str__(self):
+        return f'{self.campanha.nome} - {self.vacina.nome}'
