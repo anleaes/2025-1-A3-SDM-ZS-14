4906ac26d0088bdb36722e30f548d9faac341a12
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:27:28 -0300

    imports

--- aplicacoes_vacinais/views.py
+++ aplicacoes_vacinais/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import AplicacaoVacina
+from .serializers import AplicacaoVacinaSerializer
