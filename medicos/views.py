7316044e91cec0ba6fe3ea8b4872c50e5a253f96
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:56:21 -0300

    viewset

--- medicos/views.py
+++ medicos/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from medicos.models import Medico
 from medicos.serializers import MedicoSerializer
+
+class MedicoViewSet(viewsets.ModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = MedicoSerializer
