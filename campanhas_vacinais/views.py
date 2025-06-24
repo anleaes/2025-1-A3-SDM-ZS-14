4a3589aec58c2d5c1f104bed0e1aaa469e59d1e4
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:36:49 -0300

    viewset

--- campanhas_vacinais/views.py
+++ campanhas_vacinais/views.py
@@ -4,3 +4,7 @@ from rest_framework import viewsets
 
 from campanhas_vacinais.models import CampanhaVacinal
 from campanhas_vacinais.serializers import CamapanhaVacinalSerializer
+
+class CampanhaVacinalViewSet(viewsets.ModelViewSet):
+    queryset = CampanhaVacinal.objects.all()
+    serializer_class = CamapanhaVacinalSerializer
