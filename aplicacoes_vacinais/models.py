5b6c2e50f39d554260e0de3ae50253248c02da80
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 04:24:41 -0300

    import + 2 foreinkeys

--- aplicacoes_vacinais/models.py
+++ aplicacoes_vacinais/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class AplicacaoVacina(models.Model):
+    data_aplicacao = models.DateField()
+    dose = models.PositiveIntegerField()
+
+    medico = models.ForeignKey(
+        'medicos.Medico',
+        on_delete=models.CASCADE,
+        related_name='aplicacoes_realizadas' 
+
+    )
+
+    usuario = models.ForeignKey(
+        'usuarios.Usuario',
+        on_delete=models.CASCADE,
+        related_name='aplicacoes_recebidas'
+    )
