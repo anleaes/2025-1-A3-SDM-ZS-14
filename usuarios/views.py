d0677b691891c41460e585141f347df12e41a067
DiegoGazineu <128416494+DiegoGazineu@users.noreply.github.com>
2025-06-24 03:43:55 -0300

    viewset

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,3 +3,8 @@ from rest_framework import viewsets
 
 from usuarios.models import Usuario
 from usuarios.serializers import UsuarioSerializer
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
